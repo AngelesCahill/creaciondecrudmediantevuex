37920f76a9fda329657328bad078a78025c7cc7d
AngelesCahill <70714713+AngelesCahill@users.noreply.github.com>
2020-12-28 14:53:25 -0300

    subiendo la base

---
+++ src/components/ListadoUsuarios.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ListadoUsuarios'
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/config/configFirebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyClZbofjAepUloGPYeTXqP66jXM-pWrcs0",
+    authDomain: "ejerciciofirebase002.firebaseapp.com",
+    projectId: "ejerciciofirebase002",
+    storageBucket: "ejerciciofirebase002.appspot.com",
+    messagingSenderId: "346499483671",
+    appId: "1:346499483671:web:f8077718b23609c09ab452"
+  };
